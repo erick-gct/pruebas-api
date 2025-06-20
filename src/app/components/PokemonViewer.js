deffafc52884af4973dd93c8282a62f155c9b022
Erick Campuzano <89644016+erick-gct@users.noreply.github.com>
2025-06-20 15:15:18 -0500

    segundo commit

--- src/app/components/PokemonViewer.js
+++ src/app/components/PokemonViewer.js
@@ -3,34 +3,64 @@
 import { useEffect, useState } from "react";
 
 export default function PokemonViewer() {
-  const [pokemon, setPokemon] = useState(null);
+  const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-      .then((res) => res.json())
-      .then((data) => setPokemon(data));
-  }, []);
+    async function fetchPokemons() {
 
-  if (!pokemon) {
+        try{
+            const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+            const data = await res.json();
+
+            const details = await Promise.all(
+                data.results.map(p=>fetch(p.url).then(res => res.json()))
+                )
+            setPokemonList(details);
+
+        }catch (error) {
+          console.error("Error fetching Pokémon data:", error);
+        }
+        
+    }
+    fetchPokemons();
+    }, []);
+   
+
+
+  if (!pokemonList.length) {
     return <div>Cargando pokemonsitos...</div>;
   }
 
   return (
-    <div style={{ border: "1px solid #ccc", padding: 20, borderRadius: 10 }}>
-      <h2>{pokemon.name.toUpperCase()}</h2>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-      <p>
-        <strong> Altura: </strong>
-        {pokemon.height}
-      </p>
-      <p>
-        <strong> Peso: </strong>
-        {pokemon.weight}
-      </p>
-      <p>
-        <strong>Tipo:</strong>{" "}
-        {pokemon.types.map((t) => t.type.name).join(", ")}
-      </p>
-    </div>
+
+    <div>
+        <h2>Lista de Pokemones</h2>
+    
+    <div style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(150px, 1fr))',
+        gap: '1rem',
+      }}>
+        {pokemonList.map(pokemon => (
+            <div key= {pokemon.id} style={{
+                border: '1px solid #ccc',
+                padding: '10',
+                borderRadius: '10',
+                textAlign: 'center',
+            }}>
+
+                <h4>{pokemon.name.toUpperCase()}</h4>
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <p>Altura: {pokemon.height}</p>
+            <p>Peso: {pokemon.weight}</p>
+            </div>
+
+        ))}
+
+
+   </div>
+   </div>
+      
+
   );
 }
